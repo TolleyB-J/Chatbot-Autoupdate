fff29ae0435be1a157b666146d1c0c3e89824e1c
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-02-24 20:14:18 +0000

    Adding an npm install to add new dependencies

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cmd = require('node-cmd');
 const app = express();
 const port = 3001;
+const updateChatBotCMD = 'cd /home/ubuntu/ChatBot && git pull && npm install && pm2 restart ChatBot'
 
 app.get('/updateChatBot', (req, res) => {
-    cmd.get('cd /home/ubuntu/ChatBot && git pull && pm2 restart ChatBot', function(err, data, stderr){
+    cmd.get(updateChatBotCMD, function(err, data, stderr){
         console.log(data)
     })
     res.send('ChatBot Updating! (Use POST next time)')});
 app.post('/updateChatBot', (req, res) => {
-    cmd.get('cd /home/ubuntu/ChatBot && git pull && pm2 restart ChatBot', function(err, data, stderr){
+    cmd.get(updateChatBotCMD, function(err, data, stderr){
         console.log(data)
     })
     res.send('ChatBot Updating!')});
